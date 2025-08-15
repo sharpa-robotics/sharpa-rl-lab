ec6babdf6b7230afa9b663ffed69e6f966ffc33c
renrenyuan <renyuan.ren@sharpa.com>
2025-08-15 18:53:35 +0800

    sync

---
+++ rl_isaaclab/scripts/play.py
@@ -0,0 +1,105 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Script to train RL agent with Gym-Style agent."""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import sys
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Train an RL agent.")
+parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+parser.add_argument("--cache", type=str, default=None, help="Cache path.")
+parser.add_argument("--load_path", type=str, default=None, help="Checkpoint path.")
+parser.add_argument("--max_agent_steps", type=int, default=None, help="RL Policy training iterations.")
+parser.add_argument("--algorithm", type=str, default=None, help="Run training with multiple GPUs or nodes.")
+parser.add_argument("--resume", action="store_true", default=False, help="Resume training from checkpoint.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+args_cli, hydra_args = parser.parse_known_args()
+
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import gymnasium as gym
+import os
+import torch
+from datetime import datetime
+
+from rl_isaaclab.algo.ppo.ppo import PPO
+from rl_isaaclab.algo.padapt.padapt import ProprioAdapt
+from rl_isaaclab.wrapper.sharpa_wave_env_wrapper import GymStyleEnvWrapper
+from rl_isaaclab.wrapper.config_wrapper import ConfigWrapper
+
+from isaaclab.envs import DirectRLEnvCfg
+
+import rl_isaaclab.tasks.inhand_rotate
+from isaaclab_tasks.utils.hydra import hydra_task_config
+
+# PLACEHOLDER: Extension template (do not remove this comment)
+
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = False
+
+@hydra_task_config(args_cli.task, "gym_style_cfg_entry_point")
+def main(env_cfg: DirectRLEnvCfg, agent_cfg: dict):
+    """Train with Gym-Style agent."""
+    env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
+    agent_cfg["algorithm"]["max_agent_steps"] = args_cli.max_agent_steps if args_cli.max_agent_steps is not None else agent_cfg["algorithm"]["max_agent_steps"]
+    agent_cfg["seed"] = args_cli.seed if args_cli.seed is not None else agent_cfg['seed']
+    env_cfg.seed = agent_cfg["seed"]
+    env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
+    agent_cfg["device"] = args_cli.device if args_cli.device is not None else agent_cfg["device"]
+    agent_cfg["algo"] = args_cli.algorithm if args_cli.algorithm is not None else agent_cfg["algo"]
+    agent_cfg["load_path"] = args_cli.load_path if args_cli.load_path is not None else agent_cfg["load_path"]
+    env_cfg.randomize_pd_gains = False
+    env_cfg.randomize_friction = False
+    env_cfg.randomize_com = False
+    env_cfg.grasp_cache_path = args_cli.cache if args_cli.cache is not None else env_cfg.grasp_cache_path
+    config = ConfigWrapper(agent_cfg, env_cfg, test=True)
+
+    # specify directory for logging experiments
+    log_root_path = os.path.abspath(os.path.join("logs", "gym_style", agent_cfg["algorithm"]["experiment_name"]))
+    print(f"[INFO] Logging experiment in directory: {log_root_path}")
+    log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print(f"Exact experiment name requested from command line: {log_dir}")
+    log_dir = os.path.join(log_root_path, log_dir)
+
+    # create isaac environment
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode=None)
+
+    env = GymStyleEnvWrapper(env, clip_actions=env_cfg.clip_actions)
+    agent = eval(agent_cfg["algo"])(env, output_dir=log_dir, full_config=config)
+    # load the checkpoint
+    resume_path = agent_cfg["load_path"]
+    print(f"[INFO]: Loading model checkpoint from: {resume_path}")
+    # load previously trained model
+    agent.restore_train(resume_path)
+
+    # run training
+    agent.train()
+
+    # close the simulator
+    env.close()
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
